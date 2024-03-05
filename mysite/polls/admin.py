35326121e37ce14cf4d6e51d3b414335f2950fdc
yyysolhhh <solbaram37@gmail.com>
2024-03-05 16:50:26 +0900

    [Part7] 관리자 폼 커스터마이징

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,4 +3,11 @@ from .models import Question
 
 
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
